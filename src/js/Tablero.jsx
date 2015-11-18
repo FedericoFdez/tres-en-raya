1fe73ea4f975bd1b010fe9f04fe014fa51374b03
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-11-18 22:17:06 +0100

    Añadida cabecera con el turno

--- src/js/Tablero.jsx
+++ src/js/Tablero.jsx
@@ -1,45 +1,26 @@
 const Casilla = require("./Casilla.jsx");
 
 var Tablero = React.createClass({
-	getInitialState: function(){
-		return {
-			clicks: 0,
-			valores: [
-				['-', '-', '-'],
-				['-', '-', '-'],
-				['-', '-', '-']
-			]
-		};
-	},
-	tableroClick: function(indiceFila, indiceColumna){
-		var valores = this.state.valores;
-		var nuevoValor = 'X';
-		if(this.state.clicks % 2 === 0){
-			nuevoValor = 'O';
-		}
-		valores[indiceFila][indiceColumna] = nuevoValor;
-		this.setState({
-			clicks: this.state.clicks + 1,
-			valores: this.state.valores
-		});
+	tableroClick: function(numeroFila, numeroColumna){
+		this.props.manejadorTableroClick(numeroFila, numeroColumna);
 	},
 	render: function(){
-		let casillas = this.state.valores.map(function(valoresFila, indiceFila){
+		let casillas = this.props.valores.map(function(valoresFila, indiceFila){
 			let fila = valoresFila.map(function(valor, indiceColumna){
 				let mykey = ""+indiceFila+indiceColumna;
 				return (
-					<Casilla valor={valor} 
+					<Casilla valor={valor}
 							 indiceFila={indiceFila}
 							 indiceColumna={indiceColumna}
 							 key={mykey}
-							 manejadorClick={this.tableroClick}/>
+							 manejadorCasillaClick={this.tableroClick}/>
 				)
 			}.bind(this));
 			return (
 				<div key={"fila"+indiceFila}>
 					{fila}
 				</div>
-			)
+				)
 		}.bind(this));
 		return (
 			<div>
@@ -48,5 +29,4 @@ var Tablero = React.createClass({
 		);
 	}
 });
-
 module.exports = Tablero;
